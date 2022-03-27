b59004ff75e58e1616445e6c18c69f050f34d529
Levi21333 <mholt2338@gmail.com>
2022-03-26 22:54:48 -0400

    Micah's work so far

--- app/src/main/java/com/revature/expiration_date/Login.kt
+++ app/src/main/java/com/revature/expiration_date/Login.kt
@@ -6,14 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.revature.expiration_date.ui.theme.Expiration_DateTheme
 
 class Login : ComponentActivity() {
@@ -45,13 +51,30 @@ fun LoginScreen() {
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
             //TextField Username
+            var username by rememberSaveable{ mutableStateOf("")}
+            TextField(value = username,
+                modifier = Modifier.padding(16.dp),
+                onValueChange = {username = it},
+                placeholder = { Text(text = "Username")}
+            )
             //TextField Password
+            var password by rememberSaveable { mutableStateOf("")}
+            TextField(value = password,
+                modifier = Modifier.padding(16.dp),
+                onValueChange = {password = it},
+                placeholder = {Text(text = "Password")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(onClick = {
+
+            }) {
+               Text(text = "Login")
+            }
+
             //TextField Confirm Password
             //Button 'Login' -> Product View
-            //Something new
-            //Something new
-            //Something new good
-            //Something new good good
+
         }
     }
 }
